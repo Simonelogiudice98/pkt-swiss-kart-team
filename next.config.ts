718c5638750f10822454614b6462323b17563a75
Simonelogiudice98 <logiusimone@gmail.com>
2026-04-25 14:15:44 +0200

    rollback next config

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,10 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.pkt-kart.ch' }],
-        destination: 'https://pkt-kart.ch/:path*',
-        permanent: true,
-      },
-    ]
-  },
   images: {
     qualities: [75, 80],
     formats: ["image/avif", "image/webp"],
+
     remotePatterns: [
       {
         protocol: "https",
